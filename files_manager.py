bdd674a47eb85077f30e9af3b725a2f28b7c7296
meirkremer <marworm1@gmail.com>
2023-11-21 20:56:41 +0200

    add documentation to files_manager.py

--- files_manager.py
+++ files_manager.py
@@ -1,6 +1,7 @@
 import os.path
 import re
 import eyed3
+from typing import Union
 from logging_manager import loger
 from time import time
 from google.oauth2 import service_account
@@ -13,12 +14,36 @@ import requests
 
 
 class FilesManager:
+    """
+    Manages the download and upload of podcast files to Google Drive.
+
+    Attributes:
+    - _podcast_list (list[Podcast]): List of Podcast objects to download and upload.
+    - _db (DatabaseManager): Database manager to interact with the database.
+    """
     def __init__(self, podcast_list: list[Podcast], db: DatabaseManager):
+        """
+        Initializes the FilesManager with the provided list of podcasts and a database manager.
+
+        Parameters:
+        - podcast_list (list[Podcast]): List of Podcast objects.
+        - db (DatabaseManager): Instance of database manager.
+        """
         self._podcast_list = podcast_list
         self._db = db
 
     @staticmethod
-    def _make_valid_file_name(file_name, ext):
+    def _make_valid_file_name(file_name: str, ext: str) -> str:
+        """
+        Generates a valid file name by removing invalid characters and adding the specified extension.
+
+        Parameters:
+        - file_name (str): Original file name.
+        - ext (str): File extension.
+
+        Returns:
+        str: Valid file name with the specified extension.
+        """
         input_string = file_name.replace('_', ' ')
         pattern = r'[^a-zA-Z\dא-ת\s]'
         clean_string = re.sub(pattern, '', input_string)
@@ -27,13 +52,32 @@ class FilesManager:
         return clean_string + ext
 
     @staticmethod
-    def _get_drive_free_space(cred_path):
+    def _get_drive_free_space(cred_path: str) -> int:
+        """
+        Retrieves the free space available in Google Drive associated with the provided credentials.
+
+        Parameters:
+        - cred_path (str): Path to the JSON file containing Google Drive credentials.
+
+        Returns:
+        int: Free space available in Google Drive (in bytes).
+        """
         drive_service = build('drive', 'v3', credentials=Credentials.from_service_account_file(cred_path))
         about = drive_service.about().get(fields='storageQuota').execute()
         free_space = int(about['storageQuota']['limit']) - int(about['storageQuota']['usage'])
         return free_space
 
-    def _get_credential(self, file_size):
+    def _get_credential(self, file_size: int) -> Union[str, None]:
+        """
+        Selects a valid Google Drive credential file based on available free space.
+
+        Parameters:
+        - file_size (int): Size of the file to be uploaded.
+
+        Returns:
+        str | None: The path to the selected credential file is provided if it exists. If no credential file is found
+         or there isn't enough storage space for the given size, 'None' is returned
+        """
         cred_paths = [os.path.join('credentials', cred_name) for cred_name in os.listdir('credentials')
                       if cred_name.endswith('json')]
         for cred in cred_paths:
@@ -41,7 +85,17 @@ class FilesManager:
                 return cred
         return None
 
-    def _download_podcast(self, file_url, file_name):
+    def _download_podcast(self, file_url: str, file_name: str) -> Union[str, None]:
+        """
+        Downloads a podcast file from the provided URL.
+
+        Parameters:
+        - file_url (str): URL of the podcast file.
+        - file_name (str): Name of the podcast file.
+
+        Returns:
+        str or None: Path to the downloaded file or None if unsuccessful.
+        """
         valid_file_name = self._make_valid_file_name(file_name, '.mp3')
         file_path = f'files/{valid_file_name}'
         try:
@@ -55,14 +109,26 @@ class FilesManager:
             return None
         if response.status_code != 200:
             return None
-        chunk_size = 1024 * 1024
+        chunk_size = 1024 * 1024  # TODO: Check the optimal chunk size for download
         with open(file_path, 'wb') as f:
             for chunk in response.iter_content(chunk_size=chunk_size):
                 f.write(chunk)
         return file_path
 
     @staticmethod
-    def _upload_podcast(file_path, mime_type, description, credential_json):
+    def _upload_podcast(file_path: str, mime_type: str, description: str, credential_json: str) -> str:
+        """
+        Uploads a podcast file to Google Drive.
+
+        Parameters:
+        - file_path (str): Path to the local podcast file.
+        - mime_type (str): MIME type of the file.
+        - description (str): Description of the podcast episode.
+        - credential_json (str): Path to the JSON file containing Google Drive credentials.
+
+        Returns:
+        str: Google Drive link to the uploaded file.
+        """
         credentials = service_account.Credentials.from_service_account_file(
             credential_json,
             scopes=['https://www.googleapis.com/auth/drive']
@@ -93,20 +159,31 @@ class FilesManager:
         os.remove(file_path)
         return f"https://drive.google.com/file/d/{response['id']}/view"
 
+    # TODO: it seams that this method not necessary and could be replace by insert_podcast_file
     def _store_podcast_date(self, podcast_id, drive_link, source_link,
                             name, description, size, duration, published_date):
         self._db.insert_podcast_file(podcast_id, drive_link, source_link, name,
                                      description, size, duration, published_date)
 
     @staticmethod
-    def _get_duration(file_path):
+    def _get_duration(file_path: str) -> int:
+        """
+        Retrieves the duration of a given audio file path by eyed3
+
+        :param file_path: Path to audio file
+        :return: (int) file duration in seconds. Zero if unsuccessful get the duration
+        """
         audio_file = eyed3.load(file_path)
         if audio_file:
-            return audio_file.info.time_secs
+            return int(audio_file.info.time_secs)
         else:
             return 0
 
     def get_all_podcast(self):
+        """
+        Download, upload, and store the data into the database for all podcast episodes in podcast_list
+        :return: None
+        """
         start_all_time = time()
         for podcast in self._podcast_list:
             start_podcast_time = time()
