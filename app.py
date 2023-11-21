6da9076c422fc07b23c6b7e3775b3bb2c1dcb528
meirkremer <marworm1@gmail.com>
2023-11-21 13:53:39 +0200

    add documentation to app.py

--- app.py
+++ app.py
@@ -9,29 +9,33 @@ from send_email import send_email
 
 
 def main():
+    """
+    The main function that orchestrates the new podcast processing workflow.
+    """
+
     loger.info('start running!')
 
-    # create db manager instance
+    # Create db manager instance
     start_db_time = time()
     db = DatabaseManager()
     loger.debug(f'init db in: {(time() - start_db_time):.1f} seconds')
 
-    # get the new podcast from rss
+    # Get the new podcast from RSS
     yesterday = (datetime.now() - timedelta(days=1)).date()
     start_check_time = time()
     new_podcast = get_all_new_podcast(db, yesterday)
     loger.info(f'got {len(new_podcast)} new podcast, in {(time() - start_check_time):.1f} seconds')
 
-    # if no new podcast receives exit without sending email
+    # If no new podcast receives exit without sending email
     if len(new_podcast) == 0:
         loger.info('no newz found! exit with out sending email')
         exit()
 
-    # download the podcast and then upload to googlDrive
+    # Download the podcast and then upload to googlDrive
     downloader = FilesManager(new_podcast, db)
     downloader.get_all_podcast()
 
-    # create the email message and send it to all members
+    # Create the email message and send it to all members
     podcast_to_send = db.fetch_unsent_podcast_files()
     email_message = create_mail_message(podcast_to_send)
 
@@ -41,10 +45,14 @@ def main():
 if __name__ == '__main__':
     start_run_time = time()
     try:
+        # Execute all workflows for new podcasts
         main()
     except Exception as e:
+        # Write general exception to log file
         loger.error(e)
     except SystemExit:
+        # Write the run-time into the log file in case the exit() function is used during runtime
         loger.info(f'run time: {timedelta(seconds=(time() - start_run_time))}')
         exit()
+    # Write the run-time into the log file
     loger.info(f'run time: {timedelta(seconds=(time()-start_run_time))}')
