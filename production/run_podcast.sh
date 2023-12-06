8098450421883593f4528a46ff02ba556790910d
meirkremer <marworm1@gmail.com>
2023-12-07 00:43:50 +0200

    create shell script for production

---
+++ production/run_podcast.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to the root dir project
+cd /home/free_net_serv/send_new_podcast
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Run the script
+python app.py
